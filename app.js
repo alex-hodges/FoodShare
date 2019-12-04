27f4a514f8c9c06d1f05f8f70e25fd4344b7282e
Alex Hodges <alex.hodges1@btinternet.com>
2019-12-04 07:02:06 +0000

    server string altered

--- app.js
+++ app.js
@@ -65,4 +65,6 @@ app.use("/recipes/:id/comments", commentRoutes);
 app.use("/recipes/:id/reviews", reviewRoutes);
 
 //SERVER 
-app.listen(process.env.PORT, process.env.IP);
+app.listen(process.env.PORT, process.env.IP,function() {
+    console.log("Server started, listening on port " + process.env.PORT);
+});
